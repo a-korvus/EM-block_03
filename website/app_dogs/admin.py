7c217282b504732592f532f5c98bdf6d517a90a3
Alexander <alexander.corvus.777@gmail.com>
2025-03-09 22:00:27 +0300

    feat: app_dogs ORM models added

--- website/app_dogs/admin.py
+++ website/app_dogs/admin.py
@@ -1,3 +1,23 @@
+"""Admin panel settings for the app_dogs."""
+
 from django.contrib import admin
 
-# Register your models here.
+from app_dogs.models import Breed, Dog
+
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    """Dog Model representation in the admin panel of the site."""
+
+    list_display = "id", "name", "age", "gender",
+    list_display_links = "id", "name",
+    ordering = "id",
+
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    """Breed Model representation in the admin panel of the site."""
+
+    list_display = "id", "name", "size",
+    list_display_links = "id", "name",
+    ordering = "id",
--- website/app_dogs/models.py
+++ website/app_dogs/models.py
@@ -1,3 +1,63 @@
-from django.db import models
-from django.contrib.auth import get_user_model
+"""Django models representing entities in the database."""
 
+from django.db import models
+
+from app_dogs.utils.choises import GenderChioce, RatingChoice, SizeChioce
+
+
+class Dog(models.Model):
+    """Dog entity in the database."""
+
+    name = models.CharField(max_length=255, null=False)
+    age = models.PositiveSmallIntegerField(null=False)
+    breed = models.ForeignKey(
+        to="Breed",
+        on_delete=models.PROTECT,
+        null=True,
+        default=None,
+        related_name="dogs",
+    )
+    gender = models.CharField(
+        max_length=6,
+        choices=GenderChioce,
+        default=GenderChioce.MALE,
+        help_text="Select a gender of the dog: male or female."
+    )
+    color = models.CharField(max_length=255, default="other")
+    favorite_food = models.CharField(max_length=255, null=True, default=None)
+    favorite_toy = models.CharField(max_length=255, null=True, default=None)
+
+    def __str__(self) -> str:
+        """Set up the string representation of the Model."""
+        return f"<{self.id}> '{self.name}'"
+
+
+class Breed(models.Model):
+    """Breed entity in the database."""
+
+    name = models.CharField(max_length=255, null=False)
+    size = models.CharField(
+        max_length=6,
+        choices=SizeChioce,
+        default=SizeChioce.MEDIUM,
+    )
+    friendliness = models.PositiveSmallIntegerField(
+        choices=RatingChoice,
+        default=RatingChoice.THREE,
+    )
+    trainability = models.PositiveSmallIntegerField(
+        choices=RatingChoice,
+        default=RatingChoice.THREE,
+    )
+    shedding_amount = models.PositiveSmallIntegerField(
+        choices=RatingChoice,
+        default=RatingChoice.THREE,
+    )
+    exercise_needs = models.PositiveSmallIntegerField(
+        choices=RatingChoice,
+        default=RatingChoice.THREE,
+    )
+
+    def __str__(self) -> str:
+        """Set up the string representation of the Model."""
+        return f"<{self.id}> '{self.name}'"
